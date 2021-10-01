9e5a798f165d085c5b5c77dff92e543ec4b9d44d
ncougs <ncougan@gmail.com>
2021-10-01 16:37:36 +1000

    adding bycrpt to hash passwords on save and comparing a users password

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
 
 const UserSchema = new Schema({
 	username: String,
@@ -9,6 +10,26 @@ const UserSchema = new Schema({
 	password: String,
 	favourite_locations: [String],
 });
+
+//Hash password on save
+UserSchema.pre('save', async (next) => {
+	const currentUser = this;
+
+	// Only hash the password if it has been modified
+	if (!currentUser.isModified('password')) {
+		return next();
+	}
+
+	//Generate salt rounds hash password
+	currentUser.password = await bcrypt.hash(currentUser.password, 10);
+
+	return next();
+});
+
+//Method to compare entered password with users password
+UserSchema.methods.checkPassword = async (password) => {
+	return bcrypt.compareSync(password, this.password);
+};
 
 const User = mongoose.model('User', UserSchema);
 
