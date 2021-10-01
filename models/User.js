6a57bd8d8a0b1fd06eed26d82b75411b9f1569f9
ncougs <ncougan@gmail.com>
2021-10-01 15:50:07 +1000

    Defining User model

---
+++ models/User.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+	username: String,
+	first_name: String,
+	last_name: String,
+	email: String,
+	password: String,
+	favourite_locations: [String],
+});
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
