73bf3ec6e618d8ebe31ef96e67f8993e5ad63c88
ncougs <ncougan@gmail.com>
2021-10-05 16:13:00 +1100

    saving session post request

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,15 +1,24 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+//routes for api/users
+
+//create a new user
 router.post('/', async (req, res) => {
 	try {
 		//get user details out of req.body
 		const newUser = new User(req.body);
 
 		//save newUser to the database
-		//TO DO: FIX ERROR WHEN SAVING
 		const result = await newUser.save();
 
+		//save session
+		req.session.save(() => {
+			req.session.user_id = newUser._id;
+			req.session.logged_in = true;
+		});
+
+		//send status response
 		res.status(200).json(result);
 	} catch (err) {
 		res.status(400).json(err);
