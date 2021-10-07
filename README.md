ccd97997fdb663e52a3d519eedb33cf55ef3cd3d
ncougs <ncougan@gmail.com>
2021-10-08 10:28:12 +1100

    delete files

--- README.md
+++
@@ -1,2 +0,0 @@
-# Surf-Community-Platform
-This application will allow users to post information about their local surf spots.
--- models/index.js
+++
@@ -1,6 +0,0 @@
-const User = require('./User');
-const Comment = require('./Comment');
-const Photo = require('./Photo');
-const Video = require('./Video');
-
-module.exports = { User, Comment, Photo, Video };
